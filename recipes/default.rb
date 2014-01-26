ab622e3ac75956b33ad974e3765bd5f591c2be84
Daniel Zautner <daniel@fewbytes.com>
2014-01-26 15:12:40 +0200

    added custom elasticsearch and graphite to grafanas config file

--- recipes/default.rb
+++ recipes/default.rb
@@ -13,3 +13,14 @@ git node['grafana']['install_dir'] do
   reference node['grafana']['revision']
   action :sync
 end
+
+template "#{node['grafana']['install_dir']}/src/config.js" do
+  source "config.js.erb"
+  mode 644
+  owner "root"
+  group "root"
+  variables({
+     :elasticsearch_url => node["grafana"]["elasticsearch_url"],
+     :graphite_url => node["grafana"]["graphite_url"]
+  })
+end

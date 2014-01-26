a5aa246c111f0fc5a2a7c6c89044a2f1c3a54708
Daniel Zautner <daniel@fewbytes.com>
2014-01-26 17:41:08 +0200

    removed unused server attribute

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,4 @@
 default['grafana']['revision'] = "58dbb01e76f4412ee150e86587296de0bb83aba9"
-default['grafana']['server'] = "apache"
 default['grafana']['install_dir'] = "/opt/grafana"
 default['grafana']['repo'] = "https://github.com/torkelo/grafana.git"
 default['grafana']['elasticsearch_url'] = '"http://"+window.location.hostname+":9200"'

6a8245622a2e1c77d5e4423d71327223a813eaa1
Daniel Zautner <daniel@fewbytes.com>
2014-01-26 17:53:09 +0200

    added port to attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,3 +3,4 @@ default['grafana']['install_dir'] = "/opt/grafana"
 default['grafana']['repo'] = "https://github.com/torkelo/grafana.git"
 default['grafana']['elasticsearch_url'] = '"http://"+window.location.hostname+":9200"'
 default['grafana']['graphite_url'] =  '"http://"+window.location.hostname+":8080"'
+default['grafana']['port'] = "80"

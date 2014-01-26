a6e80d8f77d725a5bedc8b8f7f2229a2c98962f8
Daniel Zautner <daniel@fewbytes.com>
2014-01-26 12:28:30 +0200

    added install_dir attirbue

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,4 @@
 default['grafana']['revision'] = "HEAD"
 default['grafana']['server'] = "apache"
+default['grafana']['install_dir'] = "/opt/grafana"
 default['grafana']['repo'] = "git@github.com:torkelo/grafana.git"

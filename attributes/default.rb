9d2a78231e22165b521b68f420cda07eed1abd8b
Daniel Zautner <daniel@fewbytes.com>
2014-01-26 15:12:03 +0200

    fixed typo

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,3 +2,5 @@ default['grafana']['revision'] = "HEAD"
 default['grafana']['server'] = "apache"
 default['grafana']['install_dir'] = "/opt/grafana"
 default['grafana']['repo'] = "https://github.com/torkelo/grafana.git"
+default['grafana']['elasticsearch_url'] = '"http://"+window.location.hostname+":9200"'
+default['grafana']['graphite_url'] =  '"http://"+window.location.hostname+":8080"'

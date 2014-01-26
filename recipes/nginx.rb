7485e34f90481f153a02b0e2b8c8fd12a44b0fce
Daniel Zautner <daniel@fewbytes.com>
2014-01-26 16:36:35 +0200

    added nginx conf file and nginx recipe

--- recipes/nginx.rb
+++ recipes/nginx.rb
@@ -1,2 +1,12 @@
 include_recipe "grafana::default"
-include_recipe "nginx::default"
+include_recipe "nginx::source"
+
+template "#{node['nginx']['dir']}/sites-available/grafana.conf" do
+  source "nginx/grafana.conf.erb"
+  mode "0644"
+  variables({
+     :install_dir => node['grafana']['install_dir']
+  })
+end
+
+nginx_site "grafana.conf"
